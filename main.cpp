8b9d400b067786b5bd6081203aac21e77472ff31
Pradeep Nayak <pradeep1288@gmail.com>
2016-09-25 22:12:06 -0700

    adding some tests

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@ using namespace std;
 int main() {
     LRUCache<int,int> *_intCache = new LRUCache<int,int>(5);
     _intCache->put(1,1);
-    _intCache->put(1,2);
-    std::cout<< "here: " << _intCache->get(1)<< std::endl;
+    _intCache->put(2,2);
+    _intCache->put(3,2);
+    _intCache->put(4,2);
+    _intCache->put(5,2);
+    _intCache->put(6,2);
+    cout<< "Cache entry: " << _intCache->get(1)<< endl;
     return 0;
 }
